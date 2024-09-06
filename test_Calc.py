fda72c5878297e7e3db10c8052920a00b27adbf5
Uditi <72464340+uditi-12@users.noreply.github.com>
2024-09-06 12:54:26 +0530

    Update test_Calc.py

--- test_Calc.py
+++ test_Calc.py
@@ -3,11 +3,11 @@ from calc import Calculator
  
 def test_addition():
     calc = Calculator()
-    assert calc.execute('add', 15, 5) == 20
+    assert calc.execute('add', 15, 15) == 30
  
 def test_subtraction():
     calc = Calculator()
-    assert calc.execute('subtract', 10, 5) == 5
+    assert calc.execute('subtract', 10, 4) == 6
  
 def test_multiplication():
     calc = Calculator()
@@ -15,7 +15,7 @@ def test_multiplication():
  
 def test_division():
     calc = Calculator()
-    assert calc.execute('divide', 10, 5) == 2.0
+    assert calc.execute('divide', 10, 2) == 5.0
  
 def test_division_by_zero():
     calc = Calculator()
