ed92005a5dc10db81b8a32a63fdbee973fd178aa
Uditi <72464340+uditi-12@users.noreply.github.com>
2024-09-06 12:45:55 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+import pytest
+from python import Calculator
+ 
+def test_addition():
+    calc = Calculator()
+    assert calc.execute('add', 15, 5) == 20
+ 
+def test_subtraction():
+    calc = Calculator()
+    assert calc.execute('subtract', 10, 5) == 5
+ 
+def test_multiplication():
+    calc = Calculator()
+    assert calc.execute('multiply', 10, 5) == 50
+ 
+def test_division():
+    calc = Calculator()
+    assert calc.execute('divide', 10, 5) == 2.0
+ 
+def test_division_by_zero():
+    calc = Calculator()
+    assert calc.execute('divide', 10, 0) == "Error! Division by zero."
